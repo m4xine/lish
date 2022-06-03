c2ffd4ae4838c0ccd737617ac71383a0f18c3a9a
m4xine <maxined@pm.me>
2022-06-03 16:44:48 -0400

    Add CLONE macro

--- src/helpers.h
+++ src/helpers.h
@@ -3,5 +3,6 @@
 
 #define ARRAY_LEN(x) (sizeof(x) / sizeof(*x))
 #define LAMBDA(ret_type, _body) ({ ret_type _ _body _; })
+#define CLONE(x) (memcpy(malloc(sizeof(*x)), (void *)(x), sizeof(*x)))
 
 #endif
