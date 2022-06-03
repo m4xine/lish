9744f156301902c82a362aeb20dfa7903d4ab230
m4xine <maxined@pm.me>
2022-06-03 12:37:39 -0400

    Rewrite parser, second phase wip

--- src/node.h
+++ src/node.h
@@ -9,27 +9,49 @@
 typedef uint8_t node_kind_t;
 enum 
 {
-  NODE_LIST,
   NODE_NAME,
   NODE_STR,
+  NODE_KW,
+  NODE_LIST,
+  NODE_DEFUN,
+  NODE_DEVAL,
 };
 
-typedef struct
+typedef struct node_t node_t;
+struct node_t
 {
   node_kind_t kind;
   char const *begin, *end;
   union
   {
-    // NODE_LIST
-    vec_t list;
-
     // NODE_NAME
     interner_key_t key;
   
     // NODE_STR
     string_t str;
+
+    // NODE_KW
+    kw_t kw;
+
+    // NODE_LIST
+    vec_t list;
+
+    // NODE_DEFUN
+    struct 
+    { 
+      interner_key_t name;
+      vec_t params;
+      node_t *body;
+    } defun;
+
+    // NODE_DEVAL
+    struct 
+    { 
+      interner_key_t name;
+      node_t *value;
+    } deval;
   } data;
-} node_t;
+};
 
 void
 node_print_(node_t const *n, int depth)
@@ -37,12 +59,6 @@ node_print_(node_t const *n, int depth)
   printf("%*s", depth, "");
   switch (n->kind)
   {
-    case NODE_LIST: 
-      printf("List\n"); 
-      vec_t children = n->data.list;
-      for (size_t i = 0; i < children.len; ++i)
-        node_print_((node_t const *)vec_at(&children, i), depth + 2);
-      break;
     case NODE_NAME: 
       printf(
         "Name: %.*s (%zu)\n", 
@@ -53,6 +69,15 @@ node_print_(node_t const *n, int depth)
       break;
     case NODE_STR:
       printf("String: %s\n", n->data.str.raw); 
+      break;
+    case NODE_KW:
+      printf("Keyword: %s\n", kw_str(n->data.key));
+      break;
+    case NODE_LIST: 
+      printf("List\n"); 
+      vec_t children = n->data.list;
+      for (size_t i = 0; i < children.len; ++i)
+        node_print_((node_t const *)vec_at(&children, i), depth + 2);
       break;
   }
 }
--- src/parse.h
+++ src/parse.h
@@ -2,133 +2,86 @@
 #define LISH_PARSE_H
 
 #include <stdbool.h>
-
 #include "vec.h"
 #include "string.h"
 #include "source.h"
 #include "error.h"
-#include "lex.h"
 #include "token.h"
 #include "node.h"
 
-typedef uint8_t parselet_result_kind_t;
+typedef uint8_t parselet_result_kind_t; 
 enum
 {
   PARSE_OK,
   PARSE_ERR,
-  PARSE_NONE,
+  PARSE_NONE
 };
 
 typedef struct
 {
-  parselet_result_kind_t kind;
-  
-  // Optional node; Has value if kind == PARSE_OK
-  node_t opt_node;
-} parselet_result_t;
+  parselet_result_kind_t kind; 
+  node_t opt_node; 
+} parselet_result_t; 
 
-#define OK(n) (parselet_result_t) \
-  {                               \
-    .kind = PARSE_OK,             \
-    .opt_node = n                 \
-  }                       
-
-#define ERR (parselet_result_t) \
-  {                             \
-    .kind = PARSE_ERR           \
-  }
-
-#define NONE (parselet_result_t) \
-  {                              \
-    .kind = PARSE_NONE           \
-  }
-
-typedef struct 
+parselet_result_t 
+parselet_result_bind(parselet_result_t x, 
+                     parselet_result_t (*f)(node_t))
 {
-  vec_t const *toks;
-  vec_t *nodes, *errs;
-  size_t p;
-} parse_state_t;
-
-parselet_result_t
-parse_once(parse_state_t *);
-
-#define CUR(s) ((token_t const *)vec_at((vec_t *)(s)->toks, (s)->p))
-#define EXPECT(s, k) (k == CUR(s)->kind)
-#define END(s) ((s)->p >= (s)->toks->len)
-
-typedef parselet_result_t(*parselet_t)(parse_state_t *);
-#define PARSELET(name, ps) parselet_result_t name(parse_state_t *ps)
-
-PARSELET(parselet_list, s)
-{
-  if (EXPECT(s, TOK_LPAREN))
+  // Oh I miss my >>= so much.
+  switch (x.kind)
   {
-    token_t const *begin_tok = CUR(s);
-    ++s->p; 
-
-    vec_t children = vec_null(sizeof(node_t));
-    while (false == END(s) && TOK_RPAREN != CUR(s)->kind)
-    {
-      token_t const *before_plet = CUR(s);
-      parselet_result_t n = parse_once(s);
-      switch (n.kind)
-      {
-        case PARSE_OK: 
-          vec_push(&children, &n.opt_node);
-          continue;
-        case PARSE_ERR: return ERR;
-        case PARSE_NONE: 
-          {
-            error_t err = error(
-              ERR_FATAL,
-              begin_tok->begin,
-              "Expected list item"
-            );
-            vec_push(s->errs, &err);
-            return ERR;
-          } 
-      }
-    }
-
-    token_t const *end_tok = NULL;
-    if (true == END(s))
-    {
-      error_t err = error(
-        ERR_FATAL,
-        begin_tok->begin,
-        "Expected matching closing parenthesis"
-      );
-      vec_push(s->errs, &err);
-      return ERR;
-    }
-    // Skip, it's the closing right parenthesis.
-    else 
-    {
-      end_tok = CUR(s);
-      s->p++;
-    }
-
-    node_t n = (node_t) 
-      { 
-        .kind = NODE_LIST,
-        .begin = begin_tok->begin,
-        .end = end_tok->end,
-        .data.list = children
-      };
-    return OK(n);
+    case PARSE_OK: return f(x.opt_node);
+    default: return x;
   }
-
-  return NONE;
 }
 
-PARSELET(parselet_name, s)
+#define OK(n) ((parselet_result_t) \
+  {                                \
+    .kind = PARSE_OK,              \
+    .opt_node = n                  \
+  })
+
+#define ERR ((parselet_result_t) \
+  {                              \
+    .kind = PARSE_ERR            \
+  })
+
+#define NONE ((parselet_result_t) \
+  {                               \
+    .kind = PARSE_NONE            \
+  })
+
+typedef struct
 {
-  if (EXPECT(s, TOK_NAME))
+  size_t       p;
+  vec_t const *toks;
+  vec_t       *errs, 
+              *out;
+} parse_state_t; 
+
+#define CUR(s) ((token_t const *)vec_at((vec_t *)(s)->toks, (s)->p))
+#define END(s) ((s)->p >= (s)->toks->len)
+#define EXPECT_KIND(s,k) ((k) == CUR(s)->kind)
+
+typedef parselet_result_t(*p1_parselet_t)(parse_state_t *);
+typedef parselet_result_t(*p2_parselet_t)(parse_state_t *, node_t const *);
+
+parselet_result_t
+p1_parse_once(parse_state_t *s);
+
+parselet_result_t
+p2_parse(parse_state_t *s, node_t const *);
+
+#define P1(name, s) parselet_result_t name(parse_state_t *s)
+#define P2(name, s, n) parselet_result_t name(parse_state_t *s, node_t const *n)
+
+P1(p1_name, s)
+{
+  if (EXPECT_KIND(s, TOK_NAME))
   {
     token_t const *tok = CUR(s);
     ++s->p;
-   
+
     node_t n = (node_t)
       {
         .kind = NODE_NAME,
@@ -142,15 +95,15 @@ PARSELET(parselet_name, s)
   return NONE;
 }
 
-PARSELET(parselet_str, s)
+P1(p1_str, s)
 {
-  if (EXPECT(s, TOK_STR))
+  if (EXPECT_KIND(s, TOK_STR))
   {
     token_t const *tok = CUR(s);
     ++s->p;
-    
+
     node_t n = (node_t)
-      { 
+      {
         .kind = NODE_STR,
         .begin = tok->begin,
         .end = tok->end,
@@ -162,76 +115,200 @@ PARSELET(parselet_str, s)
   return NONE;
 }
 
-#undef PARSELET
-#undef EXPECT
-
-parselet_t const PARSELETS[] =
+P1(p1_kw, s)
+{
+  if (EXPECT_KIND(s, TOK_KW))
   {
-    parselet_list,
-    parselet_name,
-    parselet_str,
+    token_t const *tok = CUR(s);
+    ++s->p;
+
+    node_t n = (node_t)
+      {
+        .kind = NODE_KW,
+        .begin = tok->begin,
+        .end = tok->end,
+        .data.kw = tok->data.kw
+      };
+    return OK(n);
+  }
+
+  return NONE;
+}
+
+P1(p1_list, s)
+{
+  if (EXPECT_KIND(s, TOK_LPAREN))
+  {
+    token_t const *begin_tok = CUR(s);
+    ++s->p;
+
+    vec_t children = vec_null(sizeof(node_t));
+    while (!END(s) && !EXPECT_KIND(s, TOK_RPAREN))
+    {
+      token_t const *before_plet = CUR(s);
+      parselet_result_t res = p1_parse_once(s);
+
+      switch (res.kind)
+      {
+        case PARSE_OK:
+          vec_push(&children, &res.opt_node);
+          continue;
+        case PARSE_ERR: return res;
+        case PARSE_NONE:
+          {
+            error_t err = error(
+              ERR_FATAL,
+              begin_tok->begin,
+              "Expected list child"
+            );
+
+            vec_push(s->errs, &err);
+            return ERR;
+          }
+      }
+    }
+
+    token_t const *end_tok = NULL;
+    if (END(s))
+    {
+      error_t err = error(
+        ERR_FATAL,
+        begin_tok->begin,
+        "Expected matching closing parenthesis"
+      );
+      
+      vec_push(s->errs, &err);
+      return ERR;
+    }
+    else
+    {
+      end_tok = CUR(s);
+      ++s->p;
+    }
+
+    node_t n = (node_t)
+      {
+        .kind = NODE_LIST,
+        .begin = begin_tok->begin,
+        .end = end_tok->end,
+        .data.list = children
+      };
+    return OK(n);
+  }
+
+  return NONE;
+}
+
+p1_parselet_t const P1_PARSELETS[] = 
+  {
+    p1_name,
+    p1_str,
+    p1_kw,
+    p1_list
   };
 
 parselet_result_t
-parse_once(parse_state_t *s)
+p1_parse_once(parse_state_t *s)
 {
-  for (size_t i = 0; i < ARRAY_LEN(PARSELETS); ++i)
+  for (size_t i = 0; i < ARRAY_LEN(P1_PARSELETS); ++i)
   {
-    parselet_result_t res = PARSELETS[i](s);
+    parselet_result_t p = P1_PARSELETS[i](s);
 
-    switch (res.kind)
+    switch (p.kind)
     {
+      case PARSE_OK: 
+      case PARSE_ERR: return p;
       case PARSE_NONE: continue;
-      default: return res; 
     }
   }
 
   return NONE;
 }
 
-#undef NONE
-#undef ERR
-#undef OK
+p2_parselet_t const P2_PARSELETS[] =
+  {
+  };
+
+parselet_result_t
+p2_parse(parse_state_t *s, node_t const *n)
+{
+  for (size_t i = 0; i < ARRAY_LEN(P2_PARSELETS); ++i)
+  {
+    parselet_result_t p = P2_PARSELETS[i](s, n);
+
+    switch (p.kind)
+    {
+      case PARSE_NONE: continue;
+      default: return p;
+    }
+  }
+
+  return OK(*n);
+}
 
 int
-parse(vec_t const *toks, source_t const *src, vec_t *errs, vec_t *out)
+parse(vec_t    const *toks,
+      source_t const *src,
+      vec_t          *errs,
+      vec_t          *out)
 {
   parse_state_t s = (parse_state_t)
     {
+      .p = 0,
       .toks = toks,
-      .nodes = out,
-      .errs = errs,
-      .p = 0
+      .errs = errs
     };
 
   PARSE_REITER:
     {
-      if (END(&s)) return 0; 
+      if (END(&s)) goto PARSE_SUCCEED;
 
-      parselet_result_t res = parse_once(&s);
-      switch (res.kind)
+      parselet_result_t p = parselet_result_bind(
+        p1_parse_once(&s),
+        LAMBDA(parselet_result_t, (node_t n)
+          {
+            return p2_parse(&s, &n);
+          })
+      );
+
+      switch (p.kind)
       {
         case PARSE_OK: 
-          vec_push(out, &res.opt_node);
+          vec_push(out, &p.opt_node);
           goto PARSE_REITER;
         case PARSE_ERR: return 1;
-        case PARSE_NONE: break;
-      }
-
-      {
-        error_t err = error(
-          ERR_FATAL,
-          CUR(&s)->begin,
-          "Unexpected %s",
-          token_kind_str(CUR(&s)->kind)
-        );
-        vec_push(errs, &err);
+        case PARSE_NONE: goto PARSE_FAILED;
       }
     }
 
-  return 0;
+  PARSE_SUCCEED:
+    {
+      return 0;
+    }
+
+  PARSE_FAILED:
+    {
+      token_t const *tok = CUR(&s);
+      error_t err = error(
+        ERR_FATAL,
+        tok->begin,
+        "Unexpected %s `%.*s`",
+        token_kind_str(tok->kind),
+        (int)(tok->end - tok->begin),
+        tok->begin
+      );
+
+      vec_push(s.errs, &err);
+      return 1;
+    }
 }
 
+#undef P2
+#undef P1
+#undef OK
+#undef ERR
+#undef NONE
+#undef EXPECT_KIND
 #undef CUR
 #undef END
 
